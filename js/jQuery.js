be54a39a4f75b9d65f9b9f24e1d0ae71a67561a9
llwang8 <wangxbl56@gmail.com>
2016-07-15 16:37:56 -0400

    implement working arithmitic operations with JavaScript

--- js/jQuery.js
+++ js/jQuery.js
@@ -1,70 +1,58 @@
 //freecodecamp - JavaScript Calculator -->
 
-var number = "";
-var result = "";
-
-function process(value) {
-	document.getElementById("output").innerHTML = value;
-	if (value != "CE") {
-		switch(value){
-			case "0":
-			case "1":
-			case "2":
-			case "3":
-			case "4":
-			case "5":
-			case "6":
-			case "7":
-			case "8":
-			case "9":
-			case ".":
-				if (result.match(/%data%/i)) {
-					result.replace("/%data%/i", parseFloat(value));
-					result = parseFloat(result);
-					document.getElementById("output").innerHTML = result;
-					result = String(result);
-					number = "";
-				}else {
-					number += value;
-				}
-				break;
-			case "+":
-			case "-":
-			case "*":
-			case "/":
-			case "%":
-				result = result + value + "%data%";
-				number = ""
-				break;
-			case "AC":
-				number = "";
-				result = "";
-			case "Ans":
-			case "=":
-				document.getElementById("output").innerHTML = parseFloat(result);
-				//result = Parser.evaluate(result);
-				//result = Math.eval(result);
-				break;
-		}
-	}else {
-		if (value != "="){
-			result += value;
-		} else {
-			result.replace()  // need to replace for */% with ()
-			result = parseFloat(result)
-			document.getElementById('demo').innerHTML = result;
-
-		}
-
-	}
-
-
-}
-
 $(document).ready(function(){
 	$('button').click(function(){
-		alert($(this).text());
-		$('#output').html($(this).text());
-		//process($(this).text());
+
+		var buttonVal = $(this).text();
+		var showText = $('#output').val();
+
+
+		function getNumberIndex(index){
+			while (!isNaN(showText.charAt(index))){
+
+				if (index === 0){
+					index = -1;
+					break;
+				}
+				index -= 1;
+			}
+			return index + 1;
+		}
+
+		if (buttonVal === "CE"){
+			showText = showText.substring(0, showText.length - 1);
+		}else if(buttonVal === "=" || buttonVal === "Ans")  {
+			var i = showText.indexOf('%');
+
+			if (i < 0) {
+				showText = eval(showText);
+			}else {
+				var percentIndex = getNumberIndex(i - 1);
+				var percentTotalIndex = -1;
+
+				if (percentIndex - 2 >= 0){
+					var percentTotalIndex = getNumberIndex(percentIndex - 2);
+					alert(percentTotalIndex);
+				}
+
+				if (percentTotalIndex >= 0){
+					var percentCal = (showText.substring(percentTotalIndex, percentIndex - 1) + "*" + showText.substring(percentIndex, i) + "/100");
+					percentCal = eval(percentCal);
+					showText = eval(showText.substring(0, percentIndex) + percentCal + showText.substring(i + 1));
+				}else {
+					alert("Need a total number to applied percent on!");
+				}
+			}
+
+		}else if (buttonVal === "AC"){
+			showText = "";
+		} else {
+			showText = $('#output').val() + buttonVal;
+		}
+
+		$('#output').val(showText);
+
 	});
 });
+
+
